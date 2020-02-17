ab8d04e19d7496b6b0da8228bc06b2100fbc38f9
Tyler Tollefson <ttollefson45@gmail.com>
2020-02-17 12:11:56 -0600

    Add project files.

---
+++ Controllers/EmailInfoController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UniqueEmailService.Models;
+using UniqueEmailService.Helpers;
+
+namespace UniqueEmailService.Controllers
+{
+    [ApiController]
+    [Route("/api/UniqueEmailService")]
+    public class EmailInfoController : ControllerBase
+    {
+
+        private readonly ILogger<EmailInfoController> _logger;
+
+        public EmailInfoController(ILogger<EmailInfoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/api/UniqueEmailService/GetUniqueCount")]
+        public IActionResult GetUniqueCount()
+        {
+            try
+            {
+                return Ok(EmailInfo.getCurrent().GetUniqueCount());
+            } catch (Exception e)
+            {
+                return BadRequest($"Encountered an exception retrieving unique count {e.Message}");
+            }
+            
+        }
+
+        [HttpPost]
+        [Route("/api/UniqueEmailService/SetEmailList")]
+        public IActionResult SetEmailList(List<string> emailList)
+        {
+            EmailValidator helper = new EmailValidator();
+            if (helper.IsValidEmailList(emailList))
+            {
+                EmailInfo.getCurrent().SetEmailList(emailList);
+                EmailInfo.getCurrent().CleanseEmailList();
+                return Ok("Email list has been successfully set.");
+            } else
+            {
+                return BadRequest("Email list must contain at least one valid email.");
+            }
+        }
+    }
+}
---
+++ Models/EmailInfo.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace UniqueEmailService.Models
+{
+    public class EmailInfo
+    {
+        public List<string> EmailList { get; set; }
+        public int UniqueCount { get; set; }
+        static EmailInfo info = null;
+
+        private EmailInfo()
+        {
+            EmailList = new List<string>();
+        }
+        public static EmailInfo getCurrent()
+        {
+            if(info == null)
+            {
+                info = new EmailInfo();
+                return info;
+            } else
+            {
+                return info;
+            }
+        }
+
+        public void SetEmailList(List<string> emailList)
+        {
+            EmailList = emailList;
+        }
+
+        public void CleanseEmailList()
+        {
+            //we create an additional list because if we tried to change the element in the EmailList, C# wouldn't like it
+            List<string> cleansedList = new List<string>();
+            foreach(string email in EmailList)
+            {
+                MailAddress toCleanse = null;
+                toCleanse = new MailAddress(email);
+                string user = toCleanse.User;
+                string host = toCleanse.Host;
+
+                //if there is a + remove it and ignore all characters after it
+                if(user.IndexOf("+") != -1)
+                {
+                    user = user.Substring(0, user.IndexOf("+"));
+                }
+                //if there is a . remove it and keep everything else
+                if(user.IndexOf(".") != -1)
+                {
+                    user = Regex.Replace(user, "[@\\.]", string.Empty);
+                }
+
+                cleansedList.Add(user + "@" + host);
+            }
+
+            //Now that all emails are cleansed make our actual email list pretty
+            EmailList = cleansedList;
+        }
+
+        public int GetUniqueCount()
+        {
+            var uniqueEmails = new HashSet<string>(EmailList);
+            return uniqueEmails.Count;
+        }
+    }
+}
